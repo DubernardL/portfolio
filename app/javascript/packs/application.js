4b7c17636bf5992849ee6b1994a7cb450a19b03b
Lucas Dubernard <lucas.dubernard@hotmail.fr>
2019-12-20 18:22:42 +0100

    first page finish and skills begin

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,10 @@
+// Typed.js
+import Typed from 'typed.js';
+
+var typed = new Typed('.typed-text', {
+  strings: ["Make great apps with great people !", "Responsive development website", "Ruby / JS / HTML / CSS"],
+  typeSpeed: 60,
+  loop: true,
+  showCursor: false
+});
+// Typed.js
