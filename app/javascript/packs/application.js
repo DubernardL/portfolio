f577241fb690b8e8e6f012d7829f966075bc34ed
Lucas Dubernard <lucas.dubernard@hotmail.fr>
2019-12-22 20:04:43 +0100

    section contact begin

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@
 import Typed from 'typed.js';
 
 var typed = new Typed('.typed-text', {
-  strings: ["Make great apps with great people !", "Responsive development website", "Ruby / JS / HTML / CSS"],
+  strings: ["Make great apps with great people !", "Solutions responsives", "Ruby / JS / HTML / CSS"],
   typeSpeed: 100,
   loop: true,
   showCursor: false
